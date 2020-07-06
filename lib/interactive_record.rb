5d81cdfc58a919e6dc07020b8641ca4671536ea5
Jonathan East <jonathan_east@ymail.com>
2020-07-06 22:06:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
 
   def self.find_by(attribute_hash)
     v = attribute_hash.values.first
-    f_v = value.class == Fixnum ? v : "'#{v}'"
+    f_v = v.class == Fixnum ? v : "'#{v}'"
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = '#{f_v}'"
     DB[:conn].execute(sql)
   end
